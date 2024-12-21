646b08bb33f3f11b7f09b098af38500b02233a9a
deepak4511 <deepaksingh451181@gmail.com>
2024-12-21 09:49:01 +0530

    updated file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash:true,
+  trailingSlash: true,
 
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: '/blog/:path*',
-        destination: 'https://qloudhost.com/blog/:path*', // WordPress blog location
+        destination: 'https://blog.qloudhost.com/:path*', // Redirect to WordPress blog with path forwarding
+        permanent: true, // Set to true for a 301 redirect
       },
     ];
   },
 };
-
-export default nextConfig;
